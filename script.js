fdeea7ab58b23f7482c9fa3191a2cf6172003cad
dfsstartinglineups <fofreepicks@gmail.com>
2026-03-31 22:05:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -725,7 +725,7 @@ window.buildNewsListHtml = function(newsItems) {
                 badgeClass = 'bg-success'; badgeText = 'Available'; descText = `${pName} is off injury report and is available to play`; break;
             case 'LR':
             case 'LOCKER ROOM':
-                badgeClass = 'bg-warning text-dark'; badgeText = 'In Game Injury'; descText = `${pName} has gone to the locker room with an apparent injury to his ${rawDesc}`; break;
+                badgeClass = 'bg-warning text-dark'; badgeText = 'In Game Injury'; descText = rawDesc ? `${pName} has gone to the locker room with an apparent injury to their ${rawDesc}` : `${pName} has gone to the locker room with an apparent injury`; break;
             case '2NDHALF':
             case '2ND HALF':
                 badgeClass = 'bg-success'; badgeText = 'Starting 2nd Half'; descText = rawDesc ? `${pName} is starting 2nd Half ${rawDesc}.` : `${pName} is starting 2nd Half.`; break;
